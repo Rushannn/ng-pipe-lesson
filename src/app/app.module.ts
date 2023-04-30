21b7a3d59587d84c43b428897e35550a17b851ae
Rushannn <rushankarimov@gmail.com>
2023-04-30 18:24:18 +0300

    feat: percent, json, number, currency, slice and text formatting pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TextTransformationPipe } from './pipes/text-taransform.pipe';
+import { PipesComponent } from './components/pipes/pipes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TextTransformationPipe,
-  ],
+  PipesComponent,],
   imports: [
     BrowserModule
   ],
